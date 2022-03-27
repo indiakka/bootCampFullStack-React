fc02eab7e0c1fd24ae0a33a72fcbf9f1e8d57b4b
indiakka <78439130+indiakka@users.noreply.github.com>
2022-03-27 18:28:19 +0200

    en el router pasamos <Pagina /> como children

--- react-/src/App.js
+++ react-/src/App.js
@@ -10,25 +10,18 @@ function App() {
     <div className="container">
       <Nav />
       <Routes>
-        <Route
-          exact
-          path="/"
-          element={() => <Pagina titulo="Mascotas" entidad="mascotas" />}
-        />
-        <Route
-          path="/veterinarias"
-          element={() => (
-            <Pagina titulo="Veterinari@s" entidad="veterinarias" />
-          )}
-        />
-        <Route
-          path="/duenos"
-          element={() => <Pagina titulo="Dueñ@s" entidad="duenos" />}
-        />
-        <Route
-          path="/consultas"
-          element={() => <Pagina titulo="Consultas" entidad="consultas" />}
-        />
+        <Route exact path="/">
+          <Pagina titulo="Mascotas" entidad="mascotas" />
+        </Route>
+        <Route path="/veterinarias">
+          <Pagina titulo="Veterinari@s" entidad="veterinarias" />
+        </Route>
+        <Route path="/duenos">
+          <Pagina titulo="Dueñ@s" entidad="duenos" />
+        </Route>
+        <Route path="/consultas">
+          <Pagina titulo="Consultas" entidad="consultas" />
+        </Route>
       </Routes>
     </div>
   );
